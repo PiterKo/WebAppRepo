7cbb3d53c7c167dbb10a87aed9dd58923a165c8e
Piotr Kolodziejek <piotr.kolodziejek@outlook.com>
2018-03-08 21:25:33 +0100

    Adding properties to ApplicationUser class.

--- WebApp/WebApp/Models/ApplicationUser.cs
+++ WebApp/WebApp/Models/ApplicationUser.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNet.Identity.EntityFramework;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
@@ -12,6 +13,17 @@ namespace WebApp.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        [Display(Name = "Imię")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Miejsce zamieszkania")]
+        public string City { get; set; }
+
+        [Display(Name = "Data urodzenia")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime? DateOfBirth { get; set; }
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
