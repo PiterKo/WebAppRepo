b125e45acd75e97c54d02308281dbd13b95e13cd
Piotr Kolodziejek <piotr.kolodziejek@outlook.com>
2018-03-08 20:22:41 +0100

    Split IdentityModels on ApplicationUser and WebAppContext

---
+++ WebApp/WebApp/Models/WebAppContext.cs
@@ -0,0 +1,21 @@
+﻿using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace WebApp.Models
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection")
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
