f7366ea9dfc315a6d62dc92a6cc45b9891289650
Piotr Kolodziejek <piotr.kolodziejek@outlook.com>
2018-03-12 13:42:43 +0100

    Changes.

---
+++ WebApp/WebApp/Controllers/AdsModelsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Repo.Models;
+
+namespace WebApp.Controllers
+{
+    public class AdsModelsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: AdModels
+        public ActionResult Index()
+        {
+            var adModels = db.Ads.AsNoTracking();
+            return View(adModels.ToList());
+        }
+    }
+}
